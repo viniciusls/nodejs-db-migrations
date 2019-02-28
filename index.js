99cb8f085872b1fd73d03a76291241f4846d2871
Vinicius Silva <vinicius.silva@ibm.com>
2019-02-28 00:25:33 -0300

    Implement install migrations method to index

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
+require('dotenv').load();
+const fs = require('fs');
+
 module.exports.help = () => {
   console.log('Under development');
 };
 
 module.exports.install = () => {
+  console.log('Creating migrations structure');
+
+  if(!fs.existsSync('./migrations')) {
+      fs.mkdirSync('./migrations');
+
+      console.log('Migrations folder created at ./migrations');
+  }
+
+  if(!fs.existsSync(__dirname + '/lib/adapters/' + process.env.database.type)) {
+    throw Error('Adapter for ' + process.env.database.type + ' not found! Please check your database type config on .env.');
+  }
+
+  const dbAdapter = require(__dirname + '/lib/adapters/' + process.env.database.type);
+
   console.log('Under development');
 };
 
